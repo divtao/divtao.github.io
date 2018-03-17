a57aa15de99476d4e567e750cd8f39aab79ceebe
LoveCode-GitHub <guohaitao90@gmail.com>
2018-03-17 08:47:31 +0800

    update
    update

---
+++ analytics/analytics.js
@@ -0,0 +1,18 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-79883090-6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-79883090-6');
+</script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?db45082bcdcd27a3f38e340c2e34ed78";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
